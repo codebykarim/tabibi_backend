788cd610eeab602c9f340e805e9505c9652b2588
karimkhaled0 <kkmawe@gmail.com>
2024-11-11 15:56:32 +0200

    Add user management features: implement change password and get user functions

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -6,6 +6,8 @@ import CheckLoginService from "../services/UserServices/CheckLoginService";
 import controllerReturn from "../utils/successReturn";
 import LoginService from "../services/UserServices/LoginService";
 import RegisterService from "../services/UserServices/RegisterService";
+import ChangePasswordService from "../services/UserServices/ChangePasswordService";
+import GetUserService from "../services/UserServices/GetUserService";
 
 export const checkLogin = async (
   req: Request,
@@ -18,15 +20,15 @@ export const checkLogin = async (
     throw new AppError("Please Provide Identity Number", 400);
   }
 
-  const changedPassword = await CheckLoginService(identitynumber);
+  const { user } = await CheckLoginService(identitynumber);
 
-  return controllerReturn({ changedPassword }, req, res);
+  return controllerReturn(user, req, res);
 };
 
 export const login = async (req: Request, res: Response, body?: any) => {
   const { identitynumber, password } =
     body ?? (req.body as { identitynumber?: string; password?: string });
-
+  console.log(identitynumber, password);
   if (!identitynumber || !password) {
     throw new AppError("Please Provide Identity Number and Password", 400);
   }
@@ -50,12 +52,43 @@ export const register = async (req: Request, res: Response, body?: any) => {
     throw new AppError("Plese Provide All the Details", 400);
   }
 
-  const { user, token } = await RegisterService(
+  const { user } = await RegisterService(identitynumber, name, phone, village);
+
+  return controllerReturn({ user }, req, res);
+};
+
+export const changePasswordFirstTime = async (
+  req: Request,
+  res: Response,
+  body?: any
+) => {
+  const { identitynumber, newPassword } =
+    body ??
+    (req.body as {
+      identitynumber?: string;
+      oldPassword?: string;
+      newPassword?: string;
+    });
+
+  if (!identitynumber || !newPassword) {
+    throw new AppError("Plese Provide All the Details", 400);
+  }
+
+  const { user, token } = await ChangePasswordService(
     identitynumber,
-    name,
-    phone,
-    village
+    undefined,
+    newPassword
   );
 
   return controllerReturn({ user, token }, req, res);
+};
+
+export const getMe = async (
+  req: Request,
+  res: Response,
+  body?: any
+): Promise<Response> => {
+  const admin = await GetUserService(Number(req.user.id));
+
+  return controllerReturn(admin, req, res);
 };
