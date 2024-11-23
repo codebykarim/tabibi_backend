6e5b57abdea7b563ea74efcb5a84daa82be8f6c8
karinity0 <karimkhaledelmawe@gmail.com>
2024-11-23 19:30:47 +0200

    Remove FAQ GPT file and refactor user routes for explicit method handling

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -6,38 +6,48 @@ import isAuth from "../middleware/isAuth";
 
 const userRoutes = Router();
 
-const userMethods: { [key: string]: MethodInfo } = {
-  "check-login": {
-    controllerFunction: UserController.checkLogin,
-    httpMethod: "post",
-  },
-  login: {
-    controllerFunction: UserController.login,
-    httpMethod: "post",
-  },
-  register: {
-    controllerFunction: UserController.register,
-    httpMethod: "post",
-  },
-  "change-password": {
-    controllerFunction: UserController.changePasswordFirstTime,
-    httpMethod: "put",
-  },
-  me: {
-    controllerFunction: UserController.getMe,
-    httpMethod: "get",
-    authFunction: isAuth,
-  },
-  "update-me": {
-    controllerFunction: UserController.updateMe,
-    httpMethod: "put",
-    authFunction: isAuth,
-  },
-};
+// const userMethods: { [key: string]: MethodInfo } = {
+//   "check-login": {
+//     controllerFunction: UserController.checkLogin,
+//     httpMethod: "post",
+//   },
+//   login: {
+//     controllerFunction: UserController.login,
+//     httpMethod: "post",
+//   },
+//   register: {
+//     controllerFunction: UserController.register,
+//     httpMethod: "post",
+//   },
+//   "change-password": {
+//     controllerFunction: UserController.changePasswordFirstTime,
+//     httpMethod: "put",
+//   },
+//   me: {
+//     controllerFunction: UserController.getMe,
+//     httpMethod: "get",
+//     authFunction: isAuth,
+//   },
+//   "update-me": {
+//     controllerFunction: UserController.updateMe,
+//     httpMethod: "put",
+//     authFunction: isAuth,
+//   },
+// };
 
-const mapedMethods = init(userMethods);
+// const mapedMethods = init(userMethods);
 
-// Map the route with method query
-userRoutes.route("/auth/users/:method").all(mapedMethods);
+// // Map the route with method query
+// userRoutes.route("/auth/users/:method").all(mapedMethods);
+
+userRoutes.post("/auth/users/check-login", UserController.checkLogin);
+userRoutes.post("/auth/users/login", UserController.login);
+userRoutes.post("/auth/users/register", UserController.register);
+userRoutes.put(
+  "/auth/users/change-password",
+  UserController.changePasswordFirstTime
+);
+userRoutes.get("/auth/users/me", isAuth, UserController.getMe);
+userRoutes.put("/auth/users/update-me", isAuth, UserController.updateMe);
 
 export default userRoutes;
