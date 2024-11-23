3b15b16bc9d45651bc52a933919648a966e679c0
karinity0 <karimkhaledelmawe@gmail.com>
2024-11-23 16:41:06 +0200

    Refactor environment variables and update Docker setup for improved configuration management

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,7 +4,7 @@ admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.PROJECTID,
     clientEmail: process.env.CLIENTEMAIL,
-    privateKey: process.env.PRIVATEKEY,
+    privateKey: process.env.PRIVATEKEY!.replace(/\\n/g, "\n"),
   }),
 });
 
